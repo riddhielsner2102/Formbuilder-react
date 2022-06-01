e91141f05f8d6c81fb832e005dac91ba3036cdf0
mileesonani <96409974+mileesonani@users.noreply.github.com>
2022-06-01 18:24:59 +0530

    Add action button in reasoncode

--- src/components/pages/Sub-System/CMSS/Masters/Outcome/ReasonCode/AgGridTable/ActionButtonModal.js
+++ src/components/pages/Sub-System/CMSS/Masters/Outcome/ReasonCode/AgGridTable/ActionButtonModal.js
@@ -10,6 +10,7 @@ import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBullet
 import CopyrightOutlinedIcon from "@mui/icons-material/CopyrightOutlined";
 import Tooltip from "@mui/material/Tooltip";
 import ConfirmMessage from "../../../../../../../ReusableComp/Message/ConfirmMessage";
+import { Button } from "arms_v2.8_webui";
 
 export default function ActionModal(props) {
   const [ModalData2, SetModal2] = useState({ show: false });
@@ -29,9 +30,18 @@ export default function ActionModal(props) {
     setDeleteFlag(false)
   }
 
+  const [data, setData] = useState([])
+  const [showAction, setShowAction] = useState(false)
+  const invokeParentMethod = () => {
+    console.log('first')
+    setData([...data, props.data])
+    setShowAction(!showAction)
+  };
+  console.log('data', data)
+
   return (
     <React.Fragment>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
+      {/* <Backdrop show={props.show} clicked={props.modalClosed} /> */}
       <AddEditModel
         show={ModalData2.show}
         modalClosed={() => {
@@ -44,59 +54,71 @@ export default function ActionModal(props) {
         onCancel={() => deleteModelClose()}
         onConfirm={() => deleteModelClose()}
       />}
-      <div
-        className="Modal"
+      <Button
+        text="Actions"
         style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opcaity: props.show ? "1" : "0",
-          width: "160px",
-          backgroundColor: "#fff !important",
+          width: "80px",
+          height: "40px",
+          backgroundColor: "#01396b !important",
+          zIndex: "10",
         }}
-      >
-        <Container
-          // className={[classes.divDow].join(" ")}
+        onClick={invokeParentMethod}
+      />
+      {showAction &&
+        <div
+          className="Modal"
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            cursor: "pointer",
-            width: "160px",
-            height: "56px",
-            padding: "8px 0",
-            backgroundColor: "white",
+            // transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+            // opcaity: props.show ? "1" : "0",
+            width: "180px",
+            backgroundColor: "#fff !important",
           }}
         >
-          <Tooltip title="List Reason Code" placement="right">
-            <IconButton sx={{ height: "40px", width: "40px" }}>
-              <FormatListBulletedOutlinedIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Edit Reason Code" placement="right">
-            <IconButton
-              sx={{ height: "40px", width: "40px" }}
-              onClick={() => {
-                showNewModal();
-              }}
-            >
-              <CreateOutlinedIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Copy Reason Code" placement="right">
-            <IconButton sx={{ height: "40px", width: "40px" }}>
-              <CopyrightOutlinedIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Delete Reason Code" placement="left">
-            <IconButton
-              sx={{ height: "40px", width: "40px" }}
-              onClick={() => {
-                deleteModel();
-              }}
-            >
-              <DeleteOutlineOutlinedIcon />
-            </IconButton>
-          </Tooltip>
-        </Container>
-      </div>
+          <Container
+            // className={[classes.divDow].join(" ")}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              cursor: "pointer",
+              width: "160px",
+              height: "56px",
+              padding: "8px 0",
+              backgroundColor: "white",
+            }}
+          >
+            <Tooltip title="List Reason Code" placement="right">
+              <IconButton sx={{ height: "40px", width: "40px" }}>
+                <FormatListBulletedOutlinedIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Edit Reason Code" placement="right">
+              <IconButton
+                sx={{ height: "40px", width: "40px" }}
+                onClick={() => {
+                  showNewModal();
+                }}
+              >
+                <CreateOutlinedIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Copy Reason Code" placement="right">
+              <IconButton sx={{ height: "40px", width: "40px" }}>
+                <CopyrightOutlinedIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Delete Reason Code" placement="left">
+              <IconButton
+                sx={{ height: "40px", width: "40px" }}
+                onClick={() => {
+                  deleteModel();
+                }}
+              >
+                <DeleteOutlineOutlinedIcon />
+              </IconButton>
+            </Tooltip>
+          </Container>
+        </div>
+      }
     </React.Fragment>
   );
 }
